991ef9eac738c4e95dbdf370ea4e8f249fccf178
Franciscofernandogonzalezadad <quesquesefranfer@gmail.com>
2024-06-13 13:04:25 -0600

    # Proyecto Urban Grocers # Se buscan los PATH para usuarios y kits # Se enumeran los kits para diferenciarlos # El número permitido de caracteres (1): # El número permitido de caracteres (511): # El número de caracteres es menor que la cantidad permitida (0): # El número de caracteres es mayor que la cantidad permitida (512): # Se permiten caracteres especiales: # Se permiten espacios: # Se permiten números: # El parámetro no se pasa en la solicitud: # Se ha pasado un tipo de parámetro diferente (número):1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Proyecto Urban Grocers 
+# Se buscan los PATH para usuarios y kits
+# Se enumeran los kits para diferenciarlos
+# El número permitido de caracteres (1):
+# El número permitido de caracteres (511):
+# El número de caracteres es menor que la cantidad permitida (0): 
+# El número de caracteres es mayor que la cantidad permitida (512):
+# Se permiten caracteres especiales:
+# Se permiten espacios:
+# Se permiten números:
+# El parámetro no se pasa en la solicitud:
+# Se ha pasado un tipo de parámetro diferente (número):
+# 
+#
+#
--- configuration.py
+++ configuration.py
@@ -1,3 +1,13 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = ' https://cnt-a4d02635-c07d-4c8e-abfc-dc01ecd3e42b.containerhub.tripleten-services.com'
+CREATE_USER_PATH = '/api/v1/users'
+KITS_PATH = '/api/v1/kits'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+
+
+
+
+
